f8d42b36244c1ddfca0c12b1637053069fb10480
Sean Collins <sean.collins@outlook.com>
2023-12-27 15:56:17 -0800

    Add second Typer command

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import typer
 
-def main(name: str):
+app = typer.Typer()
+
+@app.command()
+def hello(name: str):
     print(f"Hello {name}!")
 
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye, {name}!")
+    else:
+        print(f"Bye, {name}!")
+
 if __name__ == "__main__":
-    typer.run(main)
+    app()
