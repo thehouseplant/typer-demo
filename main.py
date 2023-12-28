9b5dd8d315dfe8d1653e819698e0eee4579e2b7e
Sean Collins <sean.collins@outlook.com>
2023-12-27 16:05:27 -0800

    Add command line argument documentation

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ app = typer.Typer()
 
 @app.command()
 def hello(name: str):
+    """
+    Say hello to NAME
+    """
     print(f"Hello {name}!")
 
 @app.command()
 def goodbye(name: str, formal: bool = False):
+    """
+    Say goodbye to NAME, optionally in a formal manner via the --formal flag
+    """
     if formal:
         print(f"Goodbye, {name}!")
     else:
